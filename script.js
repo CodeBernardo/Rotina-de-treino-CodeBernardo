dab9137e68bb6e3cbebe72517822b05448eeda64
Bernardo Stein <stein.bernardo@proton.me>
2023-07-19 13:25:24 -0300

    adicionado evento que mostra a imagem relativa ao treino

--- script.js
+++ script.js
@@ -5,42 +5,47 @@ const workOuts = [
       {
         exercicio: "Aquecimento do manguito antes",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/CMG13F12/Rotacao-interna-com-cabo.jpg'
       },
       {
         exercicio: "Desenvolvimento frontal na maquina",
         series: "4x15 reps",
+        imagemTreino: 'https://i.postimg.cc/K8q6kCjf/desenvolvimento-ombros-exercicio-maquina-e-halteres.jpg'
       },
       {
         exercicio: "Desenvolvimento frontal com halter",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/K8q6kCjf/desenvolvimento-ombros-exercicio-maquina-e-halteres.jpg'
       },
       {
         exercicio: "Elevação unilateral no cabo",
         series: "4x15 reps para cada lado",
+        imagemTreino: 'https://i.postimg.cc/Dw9tPLQ4/OIP.jpg'
       },
       {
         exercicio: "Elevação lateral na maquina",
         series: "4x12 reps",
-      },
-      {
-        exercicio: "Aquecimento do manguito antes",
-        series: "4x10 reps",
+        imagemTreino: ''
       },
       {
         exercicio: "Posterior de ombro na maquina",
         series: "4x12 reps",
+        imagemTreino: 'https://i.postimg.cc/8Pfh20f3/b144cb8b81249538554f677152bcaee1.jpg'
       },
       {
         exercicio: "Tríceps corda",
         series: "4x15 reps + drop set",
+        imagemTreino: 'https://i.postimg.cc/Hs1KnR1G/12271101-Cable-Standing-One-Arm-Tricep-Pushdown-Overhand-Grip-Upper-Arms-max-1536x1261.jpg'
       },
       {
         exercicio: "Tríceps francês unilateral",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/9Qbg0fCK/press-frances-mancuerna-unilateral.jpg'
       },
       {
         exercicio: "Triceps testa barra W",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/BZ2gtvCp/OIP.jpg'
       },
     ],
   },
@@ -50,34 +55,42 @@ const workOuts = [
       {
         exercicio: "Barra fixa",
         series: "4x até a falha",
+        imagemTreino: 'https://i.postimg.cc/NMw40swn/barra-fixa-capa.jpg'
       },
       {
         exercicio: "Puxada pela frente",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/qR0sT1sV/OIP.jpg'
       },
       {
         exercicio: "Remada curvada livre",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/y8mWHsvx/OIP.jpg'
       },
       {
         exercicio: "Remada unilateral(serrote)",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/QxLTG4h6/R.jpg'
       },
       {
         exercicio: "Remada articulada",
         series: "4x10 reps + drop set",
+        imagemTreino: 'https://i.postimg.cc/C1qxc0Bw/OIP.jpg'
       },
       {
         exercicio: "Remada baixa no cabo com barra larga",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/bvn7MwTV/OIP.jpg'
       },
       {
         exercicio: "Levantemento terra parcial",
         series: "4x8 reps",
+        imagemTreino: 'https://i.postimg.cc/Nj9S9jWG/exercicio-levantamento-terra-e-stiff-Deadlifts-capa.jpg'
       },
       {
         exercicio: "Crucifixo invertido",
         series: "4x15 reps",
+        imagemTreino: 'https://i.postimg.cc/PxR1wdGh/ac325d0e68b1a2b7ae46ecedeeba309d.jpg'
       },
     ],
   },
@@ -87,6 +100,7 @@ const workOuts = [
       {
         exercicio: "Cardio",
         series: "45 minutos",
+        imagemTreino: ''
       },
     ],
   },
@@ -96,26 +110,32 @@ const workOuts = [
       {
         exercicio: "Supino reto com halter",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/RZwYvbkX/supino-reto-com-halteres.jpg'
       },
       {
         exercicio: "Supino inclinado com halter",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/rmsgRhH6/9fc498e9684a6a946064d88ca8ce0cb8.jpg'
       },
       {
         exercicio: "Crucifixo inclinado com halter",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/TPYBQJs0/crucifixo-inclinado.jpg'
       },
       {
-        exercicio: "Crossover",
+        exercicio: "Cross over",
         series: "4x15 + drop set",
+        imagemTreino: 'https://i.postimg.cc/L5zymJ7h/574f5a1014923424e6841253b78731fc.jpg'
       },
       {
         exercicio: "Elevaçao de perna",
         series: "4x12 reps",
+        imagemTreino: 'https://i.postimg.cc/jdx6D3nM/2246d5af1a31c86930871c89ea03ce6b.jpg'
       },
       {
         exercicio: "Abdominal com anilha",
         series: "4x12 reps",
+        imagemTreino: 'https://i.postimg.cc/527WhrLT/OIP.jpg'
       },
     ],
   },
@@ -125,30 +145,37 @@ const workOuts = [
       {
         exercicio: "Rosca scott",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/PJ2SCgXk/OIP.jpg'
       },
       {
         exercicio: "Rosca alternada",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/Xq786sw1/e1e117e21a97ea243399a2cf3b18b28a.jpg'
       },
       {
         exercicio: "Rosca direta",
         series: " 4x8 reps + drop set",
+        imagemTreino: 'https://i.postimg.cc/q7T2dmrc/OIP.jpg'
       },
       {
         exercicio: "Rosca martelo",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/LXyPL21v/bfb1dca85fa1869efe0e6976355e64a1.jpg'
       },
       {
         exercicio: "Rosca punho",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/FHtLjPYt/rosca-punha-pegada-sem-polegar.jpg'
       },
       {
         exercicio: "Gêmeos em pé",
         series: "4x12 reps + drop set",
+        imagemTreino: 'https://i.postimg.cc/Mp8ncvLq/OIP.jpg'
       },
       {
         exercicio: "Gêmeos sentado",
         series: "4x12 reps",
+        imagemTreino: 'https://i.postimg.cc/dVqC05DJ/OIP.jpg'
       },
     ],
   },
@@ -158,26 +185,32 @@ const workOuts = [
       {
         exercicio: "Extensora",
         series: "4x12 reps",
+        imagemTreino: 'https://i.postimg.cc/Nf2J4fxK/a5553743b905397cea53a1608602a9a0.jpg'
       },
       {
         exercicio: "Agachamento livre",
         series: "4x8 reps",
+        imagemTreino: 'https://i.postimg.cc/bNbXvRWV/R.jpg'
       },
       {
         exercicio: "Legpress",
         series: "4x8 reps",
+        imagemTreino: 'https://i.postimg.cc/rF5bTwRv/7b67fc28fe2c712da6c8ded2ea40945e.jpg'
       },
       {
         exercicio: "Passada com halter",
         series: "4x ida e volta",
+        imagemTreino: 'https://i.postimg.cc/brpn2gL0/e25b961f0818b44960328c9c2ab1e4eb.jpg'
       },
       {
         exercicio: "Mesa flexora",
         series: "4x10 reps + drop set",
+        imagemTreino: 'https://i.postimg.cc/JnWD9Kww/2edbed-c4fbbaece45c4acfbe336069f1804b7d-mv2.gif'
       },
       {
         exercicio: "Stiff",
         series: "4x10 reps",
+        imagemTreino: 'https://i.postimg.cc/8cz5S0tY/exercicio-stiff.jpg'
       },
     ],
   },
@@ -196,11 +229,11 @@ for (let i = 0; i < workOuts.length; i++) {
   section.append(h2);
   main.appendChild(section);
 
-  const sectionZero = document.getElementById("dia_" + [i]);
-  const sectiontittle = sectionZero.querySelector("h2");
+  const sectionId = document.getElementById("dia_" + [i]);
+  const sectiontittle = sectionId.querySelector("h2");
 
   sectiontittle.addEventListener("click", function (event) {
-    const allElements = sectionZero.querySelectorAll("div");
+    const allElements = sectionId.querySelectorAll("div");
 
     for (let i = 0; i < allElements.length; i++) {
       allElements[i].classList.toggle("hidden");
@@ -208,20 +241,33 @@ for (let i = 0; i < workOuts.length; i++) {
   });
 
   for (let j = 0; j < workOuts[i].treinoDia.length; j++) {
+
     const div = document.createElement("div");
     div.classList.add("row", "hidden");
-
+    
+    const imgContainer = document.createElement('figure')
+    imgContainer.classList.add('img-container', 'hidden')
+    
+    const img = document.createElement('img')
+    img.src = workOuts[i].treinoDia[j].imagemTreino
+    
+    imgContainer.appendChild(img)
     section.appendChild(div);
-
+    
     const p = document.createElement("p");
     p.innerText = `${workOuts[i].treinoDia[j].exercicio}: ${workOuts[i].treinoDia[j].series}`;
+    
 
+    p.addEventListener('click', function(event) {
+      imgContainer.classList.toggle('hidden')
+    })
+    
     const label = document.createElement("label");
     const input = document.createElement("input");
     input.type = "checkbox";
-
+    
     label.appendChild(input);
-    div.append(p, label);
+    div.append(p, label,imgContainer);
   }
 }
 
